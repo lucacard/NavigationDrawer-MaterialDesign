9574c302131be8d400be28aa91aa4b94fd9a4787
unknown <l.cardinali@nb-cardinaliL.apra.it>
2015-01-16 10:32:55 +0100

    modifica

--- app/src/main/java/br/liveo/ndrawer/MainActivity.java
+++ app/src/main/java/br/liveo/ndrawer/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends NavigationLiveo implements NavigationLiveoList
     @Override
     public void onUserInformation() {
         //User information here
-        this.mUserName.setText("Rudson Lima modificato");
+        this.mUserName.setText("Rudson Lima modificato da branch3");
         this.mUserEmail.setText("rudsonlivemodificato@gmail.com");
         this.mUserPhoto.setImageResource(R.drawable.ic_rudsonlive);
         this.mUserBackground.setImageResource(R.drawable.ic_user_background);
